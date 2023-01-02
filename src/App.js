3bd9d57d1bb1fccfb11fb7489bec95c2e5380bdb
HadaTechnical <hadakishan407@gmail.com>
2023-01-02 10:38:14 +0530

    universal path home page

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
         <Route  path='/about' element={<About/>}/>
         <Route  path='/contact' element={<Contact/>}/>
         <Route  path='/error' element={<Error/>}/>
-        <Route  path='/*' element={<Navigate to="/error"/>}/>    
+        <Route  path='/*' element={<Navigate to="/"/>}/>    
       </Routes>
       <Footer/>
 
